9108de26ff7af06f6369be8646a2994f10449fc5
justanobody2107 <andreadallara.2107@gmail.com>
2022-04-12 14:26:48 -0400

    For now added typing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,56 @@
 let valuesObj = {
-    '[g]' : { 'sliceAmount' : 3, 'id' : 'a'},
-    '[r]' : { 'sliceAmount' : 3, 'id' : 'b'},
-    '[b]' : { 'sliceAmount' : 3, 'id' : 'c'},
-    '[link]' : { 'sliceAmount' : 6, 'id' : ''},
-    '[br]' : { 'sliceAmount' : 4, 'id' : ''},
+    '[g]': { 'sliceAmount': 3, 'id': 'a' },
+    '[r]': { 'sliceAmount': 3, 'id': 'b' },
+    '[b]': { 'sliceAmount': 3, 'id': 'c' },
+    '[link]': { 'sliceAmount': 6, 'id': '' },
+    '[br]': { 'sliceAmount': 4, 'id': '' },
 }
 
 let values = new Map(Object.entries(valuesObj));
 
-function addChar(char, element){
+function addChar(char, element) {
     element.innerHTML += char;
 }
 
-function removeChar(element){
-    element.innerHTML = element.innerHTML.slice(0, element.innerHTML.length-1);
+function removeChar(element) {
+    element.innerHTML = element.innerHTML.slice(0, element.innerHTML.length - 1);
 }
 
-function parseText(textarr) {
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+async function parseText(textarr) {
     for (let line of textarr) {
         let value = /\[.{1,99}\]/.exec(line);
         if (value == null || value.index != 0) {
             let element = document.getElementById('console');
-            line = line.slice(0, line.length-1);
             for (let char of line) {
                 addChar(char, element);
                 continue;
             }
             continue;
-        }
-        else if (values.has(value[0])) {
+        } else if (values.has(value[0])) {
             let obj = values.get(value[0]);
             let id = obj.id;
             let style = value[0].slice(1, -1);
             let type = 'span';
             let link = null;
             line = line.slice(obj.sliceAmount);
-            if(style == "link") {
+            if (style == "link") {
                 link = /\(.{1,99}\)/.exec(line);
                 line = line.slice(link[0].length);
                 type = 'a';
-            }else if(style == 'br') {
+            } else if (style == 'br') {
                 type = 'br';
             }
             let element = document.createElement(type);
             document.getElementById('console').appendChild(element);
             element.setAttribute('id', id);
-            if(link){
-                element.setAttribute('href', link[0].slice(1, link[0].length-1));
+            if (link) {
+                element.setAttribute('href', link[0].slice(1, link[0].length - 1));
             }
-            line = line.slice(0, line.length-1);
-            for (let char of line){
-                addChar(char, element);
-                // ok so apparently idk how git commits work so here ya go trop this is just for u bbg
+            for (let i = 0; i < line.length; i++) {
+                addChar(line[i], element);
+                await timer(Math.random() * 20 + 20);
                 continue;
             }
             continue;
@@ -59,7 +58,7 @@ function parseText(textarr) {
     };
 }
 
-function getFile(file){
+function getFile(file) {
     $.get(file, function(data) {
         let textarr;
         textarr = data.split('\n');
@@ -67,7 +66,13 @@ function getFile(file){
     });
 }
 
-getFile('assets/txt/console.txt');
+async function preFab() {
+    getFile('assets/txt/console.txt').then(() => {
+        getFile('assets/txt/justanobody.txt');
+    });
+}
+
+preFab();
 
 let addedCount = 0;
 
@@ -76,13 +81,13 @@ function addText(key) {
     let keyCodes = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 32];
     let specialCodes = [13, 8];
     let text;
-    if(keyCodes.includes(key.keyCode)){
+    if (keyCodes.includes(key.keyCode)) {
         text = key.key;
         addChar(text, element);
         addedCount++;
-    }else if(specialCodes.includes(key.keyCode)){
-        if(key.keyCode == 8){
-            if(addedCount > 0){
+    } else if (specialCodes.includes(key.keyCode)) {
+        if (key.keyCode == 8) {
+            if (addedCount > 0) {
                 removeChar(element);
                 addedCount--;
             }
@@ -90,4 +95,4 @@ function addText(key) {
     }
 }
 
-document.onkeydown=addText;
+document.onkeydown = addText;
