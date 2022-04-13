db60cb79ab09a80297305270e66f8604b29ac2e7
justanobody2107 <andreadallara.2107@gmail.com>
2022-04-13 09:32:49 -0400

    Added blinking cursor + fixed typing during output

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ let valuesObj = {
 }
 
 let values = new Map(Object.entries(valuesObj));
+let allowType = false;
 
 function addChar(char, element) {
     element.innerHTML += char;
@@ -19,16 +20,17 @@ function removeChar(element) {
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
 async function parseText(textarr) {
+    allowType = false;
     for (let line of textarr) {
         let value = /\[.{1,99}\]/.exec(line);
         if (value == null || value.index != 0) {
             let element = document.getElementById('console');
             for (let char of line) {
                 addChar(char, element);
-                let randTest = Math.random()*100;
-                if(randTest < 30){
+                let randTest = Math.random() * 100;
+                if (randTest < 30) {
                     await timer(30);
-                }else{
+                } else {
                     await timer(Math.random() * 10 + 5);
                 }
                 continue;
@@ -56,10 +58,10 @@ async function parseText(textarr) {
             }
             for (let i = 0; i < line.length; i++) {
                 addChar(line[i], element);
-                let randTest = Math.random()*100;
-                if(randTest < 30){
+                let randTest = Math.random() * 100;
+                if (randTest < 30) {
                     await timer(30);
-                }else{
+                } else {
                     await timer(Math.random() * 10 + 5);
                 }
                 continue;
@@ -68,11 +70,22 @@ async function parseText(textarr) {
             continue;
         }
     };
+    allowType = true;
+}
+
+function blinkCursor() {
+    var cont = document.getElementById('console');
+    if (cont.innerHTML.includes('|')) {
+        cont.innerHTML = cont.innerHTML.replace('|', '');
+    } else {
+        cont.innerHTML += "|";
+    }
 }
 
 async function getFile(file) {
     let textarr;
-    function changeText(value){
+
+    function changeText(value) {
         textarr = value;
     }
     $.get(file, function(data) {
@@ -82,51 +95,71 @@ async function getFile(file) {
     await parseText(textarr);
 }
 
+let blinkCursorInterval;
+
 async function preFab() {
     await getFile('assets/txt/console.txt');
     await getFile('assets/txt/justanobody.txt');
     await getFile('assets/txt/console.txt');
     await getFile('assets/txt/commands.txt');
     await getFile('assets/txt/console.txt');
+    blinkCursorInterval = setInterval(blinkCursor, 500);
 }
 
 preFab();
 
 let addedCount = 0;
 
-function addText(key) {
-    let element = document.getElementById('console');
-    let keyCodes = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 32];
-    let specialCodes = [13, 8];
-    let text;
-    if (keyCodes.includes(key.keyCode)) {
-        text = key.key;
-        addChar(text, element);
-        addedCount++;
-    } else if (specialCodes.includes(key.keyCode)) {
-        if (key.keyCode == 8) {
-            if (addedCount > 0) {
-                removeChar(element);
-                addedCount--;
+async function addText(key) {
+    if (allowType) {
+        let element = document.getElementById('console');
+        let keyCodes = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 32];
+        let specialCodes = [13, 8];
+        let text;
+        if (keyCodes.includes(key.keyCode)) {
+            clearTimeout(blinkCursorInterval);
+            if (element.innerHTML.includes('|')) {
+                element.innerHTML = element.innerHTML.replace('|', '');
             }
-        } else if (key.keyCode == 13) {
-            let command = element.innerHTML.slice(element.innerHTML.length - addedCount, element.innerHTML.length);
-            if (command == "help") {
-                getFile('assets/txt/help.txt');
-                addedCount = 0;
-            } else if (command == "matrix") {
-                window.open("https://chat.justanobody.live/", "_self");
-            } else if (command == "socials") {
-                getFile('assets/txt/socials.txt');
-                addedCount = 0;
-            } else if (command == "portfolio") {
-                window.open("https://real.justanobody.live/", "_self");
-            } else if (command == "discord") {
-                window.open("https://discord.gg/CxtxXUcEfC", "_self");
-            } else {
-                element.innerHTML += "<br/><br/>Unknown command: " + command + "<br/><br/>";
-                getFile('assets/txt/console.txt');
-                addedCount = 0;
+            text = key.key;
+            addChar(text, element);
+            addedCount++;
+            blinkCursorInterval = setInterval(blinkCursor, 500);
+        } else if (specialCodes.includes(key.keyCode)) {
+            if (key.keyCode == 8) {
+                clearTimeout(blinkCursorInterval);
+                if (element.innerHTML.includes('|')) {
+                    element.innerHTML = element.innerHTML.replace('|', '');
+                }
+                if (addedCount > 0) {
+                    removeChar(element);
+                    addedCount--;
+                }
+                blinkCursorInterval = setInterval(blinkCursor, 500);
+            } else if (key.keyCode == 13) {
+                clearTimeout(blinkCursorInterval);
+                if (element.innerHTML.includes('|')) {
+                    element.innerHTML = element.innerHTML.replace('|', '');
+                }
+                let command = element.innerHTML.slice(element.innerHTML.length - addedCount, element.innerHTML.length);
+                if (command == "help") {
+                    await getFile('assets/txt/help.txt');
+                    addedCount = 0;
+                } else if (command == "matrix") {
+                    window.open("https://chat.justanobody.live/", "_self");
+                } else if (command == "socials") {
+                    await getFile('assets/txt/socials.txt');
+                    addedCount = 0;
+                } else if (command == "portfolio") {
+                    window.open("https://real.justanobody.live/", "_self");
+                } else if (command == "discord") {
+                    window.open("https://discord.gg/CxtxXUcEfC", "_self");
+                } else {
+                    await parseText(["[br]", "[br]", "Unknown command: " + command, "[br]", "[br]"]);
+                    await getFile('assets/txt/console.txt');
+                    addedCount = 0;
+                }
+                blinkCursorInterval = setInterval(blinkCursor, 500);
             }
         }
     }
